a8eb7e9d6cfed06d61c091dce457b68cd21a49f2
clabrocheAddWorking <clabrocheAddWorking@users.noreply.github.com>
2024-11-13 23:52:10 +0100

    ci: update workflow

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/with-contenv bashio
+TUYA_SECRET_KEY=$(jq -r '.TUYA_SECRET_KEY' /data/options.json)
+TUYA_ACCESS_KEY=$(jq -r '.TUYA_ACCESS_KEY' /data/options.json)
+APIKEY=$(jq -r '.APIKEY' /data/options.json)
+
+# Définir les variables d'environnement pour le reste du script
+export TUYA_SECRET_KEY
+export TUYA_ACCESS_KEY
+export APIKEY
+
 node src/index.js
